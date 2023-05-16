10f5843ff357356b15d9cb7217d88ca658c56bfe
Mateo Diaz <mateodd@me.com>
2023-05-15 20:00:17 -0700

    Plotting infrastructure

---
+++ experiments/generate_figures.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+def extract_avg_min_max(data, method, l=None):
+    chopped_data = [ser[:l] for ser in data[method]]
+    return np.mean(chopped_data, 0), np.min(chopped_data, 0), np.max(chopped_data, 0)
+
+
+def generate_figures(results_path):
+    data = pickle.load(open(os.path.join(results_path, "state.p"), "rb"))
+    dimensions = data["dimensions_to_extend"]
+    avg_compatible, min_compatible, max_compatible = extract_avg_min_max(
+        data["test_error_across_dim"], "compatible"
+    )
+    avg_free, min_free, max_free = extract_avg_min_max(
+        data["test_error_across_dim"], "free"
+    )
+
+    with plt.style.context(["science"]):
+        fig, ax = plt.subplots()
+        ax.plot(dimensions, avg_free, label="Free", color="#ffb000", linestyle="dashed")
+        ax.fill_between(
+            dimensions, min_free, max_free, color="#ffb000", linewidth=0.0, alpha=0.2
+        )
+        ax.plot(dimensions, avg_compatible, label="Compatible", color="#785ef0")
+        ax.fill_between(
+            dimensions,
+            min_compatible,
+            max_compatible,
+            color="#785ef0",
+            linewidth=0.0,
+            alpha=0.2,
+        )
+        plt.yscale("log")
+        ppar = dict(xlabel=r"Dimension $d$", ylabel=r"Mean squared error")
+        ax.legend()
+        ax.set(**ppar)
+        plt.savefig(os.path.join(results_path, "interdimensional_test_error.pdf"))
+
+    l_compatible = min([len(l) for l in data["train_losses"]["compatible"]])
+    avg_compatible, min_compatible, max_compatible = extract_avg_min_max(
+        data["train_losses"], "compatible", l_compatible
+    )
+    # l_free = min([len(l) for l in data["train_losses"]["free"]])
+    # avg_free, min_free, max_free = extract_avg_min_max(data["train_losses"], "free", l_free)
+
+    with plt.style.context(["science"]):
+        fig, ax = plt.subplots()
+        # ax.plot(range(l_free), avg_free, label="Free", color="#ffb000", linestyle="dashed")
+        # ax.fill_between(
+        # range(l_free), min_free, max_free, color="#ffb000", linewidth=0.0, alpha=0.2
+        # )
+        ax.plot(
+            range(l_compatible), avg_compatible, label="Compatible", color="#785ef0"
+        )
+        ax.fill_between(
+            range(l_compatible),
+            min_compatible,
+            max_compatible,
+            color="#785ef0",
+            linewidth=0.0,
+            alpha=0.2,
+        )
+        plt.yscale("log")
+        ppar = dict(xlabel=r"Iteration count", ylabel=r"Loss function")
+        ax.legend()
+        ax.set(**ppar)
+        plt.savefig(os.path.join(results_path, "convergence.pdf"))
+
+
+if __name__ == "__main__":
+    results_path = "results/trace/06:13PM-May-15-2023"
+    generate_figures(results_path)
--- experiments/symmetric_projection.py
+++ experiments/symmetric_projection.py
@@ -1,48 +1,96 @@
 #!/usr/bin/env python3
+from oil.utils.utils import FixedNumpySeed, FixedPytorchSeed
+import datetime
+import pickle
+import os
+import numpy as np
 import logging
 from emlp.datasets import SymmetricProjection
 from emlp.nn import EMLPSequence
 from trainer.model_trainer import RegressorPlus
-from utils import generate_datasets_in_dimensions, get_data_loaders
 from oil.datasetup.datasets import split_dataset
 import emlp.nn
 import emlp.reps
 import emlp.groups
 from emlp.reps import PermutationSequence
 import objax
+from utils import (
+    generate_data_train_and_test,
+)
 
+from generate_figures import generate_figures
+
+
+# Data generation mechanism
+def random_sample(size):
+    return np.random.randn(size, size)
+
+
+def true_mapping(x):
+    y = (x + x.T) / 2
+    return y
+
+
+# Parameters
+logging.getLogger().setLevel(logging.INFO)
 seed = 926
-ndata = 4000
-batch_size = 600
-num_epochs = 1000
-step_size = lambda e: 2e-3
-dimensions_to_extend = list(range(2, 11))
+n_train = 3000
+n_test = 1000
+dimensions_to_extend = list(range(2, 16))
 learning_dimension = 4
-num_hidden_layers = 2
+num_rep = 3
+solver_config = {
+    "step_size": 8e-3,
+    "num_epochs": 300,
+    "batch_size": 500,
+    "tolerance": 1e-8,
+}
 
-
+# Architecture
 T1 = PermutationSequence()
 seq_in = T1 * T1
 inner = 4 * T1 + 4 * seq_in
 seq_out = seq_in
-free_NN = EMLPSequence(seq_in, seq_out, num_hidden_layers * [inner], is_compatible=True)
-model = free_NN.emlp_at_level(learning_dimension)
+num_hidden_layers = 2
 
-log_level = "info"
-split = {"train": -1, "val": 1000, "test": 1000}
-trainer_config = {
-    "log_dir": None,
-    "log_args": {"minPeriod": 1, "timeFrac": 0.9},
-    "early_stop_metric": "val_MSE",
-}
-
-datasets = generate_datasets_in_dimensions(
-    SymmetricProjection, dimensions_to_extend, ndata, seed=seed
+# Generate data, train, and test
+train_losses, times_to_extend, test_error_across_dim = generate_data_train_and_test(
+    seed,
+    dimensions_to_extend,
+    random_sample,
+    true_mapping,
+    learning_dimension,
+    n_train,
+    n_test,
+    seq_in,
+    seq_out,
+    inner,
+    num_hidden_layers,
+    solver_config,
+    num_rep,
 )
-learning_datasets = split_dataset(datasets[learning_dimension], splits=split)
-dataloaders = get_data_loaders(batch_size, learning_datasets)
 
-solver = objax.optimizer.Adam
-logging.getLogger().setLevel(logging.INFO)
-trainer = RegressorPlus(model, dataloaders, solver, step_size, **trainer_config)
-trainer.train(num_epochs)
+# Save and generate figures
+state = dict(
+    seq_in=seq_in,
+    inner=inner,
+    seq_out=seq_out,
+    seed=seed,
+    n_train=n_train,
+    n_test=n_test,
+    dimensions_to_extend=dimensions_to_extend,
+    learning_dimension=learning_dimension,
+    num_hidden_layers=num_hidden_layers,
+    num_rep=num_rep,
+    solver_config=solver_config,
+    train_losses=train_losses,
+    times_to_extend=times_to_extend,
+    test_error_across_dim=test_error_across_dim,
+)
+folder_name = datetime.datetime.now().strftime("%I:%M%p-%B-%d-%Y")
+results_directory = os.path.join("results", "symmetric_projection", folder_name)
+results_path = os.path.join(results_directory, "state.p")
+os.mkdir(results_directory)
+pickle.dump(state, open(results_path, "wb"))
+generate_figures(results_directory)
+print(f"The results were saved on: {results_directory}")
