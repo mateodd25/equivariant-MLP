f64819009d31bc395c234fe9719c79abdf3b4bfd
Mateo Diaz <mateodd@me.com>
2023-05-16 17:05:55 -0700

    Add reasonable generators back in

--- emlp/groups.py
+++ emlp/groups.py
@@ -344,14 +344,14 @@ class S(Group):  # The permutation group
     def __init__(self, n):
         # Here we choose n-1 generators consisting of swaps between the first element
         # and every other element
-        perms = np.arange(n)[None] + np.zeros((n - 1, 1)).astype(int)
-        perms[:, 0] = np.arange(1, n)
-        perms[np.arange(n - 1), np.arange(1, n)[None]] = 0
-        # perms = np.arange(n)[None] + np.zeros((2, 1)).astype(int)
-        # perms[0][0] = 1
-        # perms[0][1] = 0
-        # perms[1][:-1] = np.arange(1, n)
-        # perms[1][-1] = 0
+        # perms = np.arange(n)[None] + np.zeros((n - 1, 1)).astype(int)
+        # perms[:, 0] = np.arange(1, n)
+        # perms[np.arange(n - 1), np.arange(1, n)[None]] = 0
+        perms = np.arange(n)[None] + np.zeros((2, 1)).astype(int)
+        perms[0][0] = 1
+        perms[0][1] = 0
+        perms[1][:-1] = np.arange(1, n)
+        perms[1][-1] = 0
         self.discrete_generators = [LazyPerm(perm) for perm in perms]
         super().__init__(n)
         # We can also have chosen the 2 generator soln described in the paper, but
